aea3f78aacd48ea049191a0e24a5a924e05fc29a
Steven Casper <sebastiancasper3@gmail.com>
2021-08-21 14:01:40 -0400

    code: basic setup

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::BLUE))
+        .add_startup_system(setup)
         .add_system(bevy::input::system::exit_on_esc_system)
         .run()
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
